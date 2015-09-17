9893169447cc3bd1e4626538ca796451c11337cf
BaeJuongEun <wjddms5810@naver.com>
2015-09-18 00:15:31 +0900

    review in my home

---
+++ src/com/j2/wk2/Simulator.java
@@ -0,0 +1,11 @@
+package com.j2.wk2;
+
+public class Simulator{
+ 
+	public static void main(String[] args) {
+ 
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+	}
+}
