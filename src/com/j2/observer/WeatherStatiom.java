181a0442d4d1608789266ee8a5448d354ac186a7
wjddms5810 <wjddms5810@naver.com>
2015-09-24 20:41:38 +0900

    Delete WeatherStatiom.java

--- src/com/j2/observer/WeatherStatiom.java
+++
@@ -1,9 +0,0 @@
-package com.j2.observer;
-
-public class WeatherStatiom{
-  public static void main(String[] ars){
-    WeatherData weatherData  = new WeatherData();
-    CureentConditionsDisplay currentDisplay = new CureentConditionsDisplay (weatherData);
-    weatherData.setMeasurements(80,65,30.4f);
-  }
-}
