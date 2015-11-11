0b5bd75ea18dcc2576486b4966530a4a73abdeac
unknown <So-yeon>
2015-11-11 13:49:41 +0900

    11.11

---
+++ src/com/j2/adapter/ducks/TurkeyAdapter.java
@@ -0,0 +1,16 @@
+package com.j2.adapter.ducks;
+
+public class TurkeyAdapter implements Duck{
+  Turkey turkey;
+  public TurkeyAdapter(Turkey turkey){
+    this.turkey = turkey;
+  }
+  public void quack(){
+    turkey.gobble();
+  }
+  public void fly(){
+    for(int i=0; i<3; i++){
+    turkey.fly();
+    }
+  }
+}
---
+++ src/com/j2/facade/hometheater/simple/Amplifier.java
@@ -0,0 +1,30 @@
+package com.j2.facade.hometheater.simple;
+
+public class Amplifier{
+  private String description;
+  private Tuner tuner;
+  private DvdPlayer dvd;
+  private int level;
+  
+  public Amplifier(String description){
+    this.description = description;
+  }
+  public void on(){
+    System.out.println(description+ "on");
+  }
+  public void off(){
+    System.out.println(description+ "off");
+  }
+  public void setDvd(DvdPlayer dvd){
+    System.out.println(description+ "setting dvd");
+    this.dvd = dvd;
+  }
+  public void setTuner(Tuner tuner){
+    System.out.println(description+ "setting tuner");
+    this.tuner = tuner;
+  }
+  public void setVolume(int level){
+    System.out.println(description+ "setting level");
+    this.level = level;
+  }
+}
