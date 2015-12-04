55efd7875ce1bf511fb380ea01a9e60d45d51d53
BaeJuongEun <wjddms5810@naver.com>
2015-12-04 22:49:04 +0900

    12.04

---
+++ src/com/j2/state/gumballstatewinner/WinnerState.java
@@ -0,0 +1,35 @@
+package com.j2.state.gumballstatewinner;
+
+public class WinnerState implements State{
+	GumballMachine gumballMachine;
+	
+	public WinnerState(GumballMachine gumballMachine){
+		this.gumballMachine = gumballMachine;
+	}
+	public void insertQuarter(){
+		System.out.println("Please wait, we're already giving you a gumball");
+	}
+	public void ejectQuarter(){
+		System.out.println("Please wait, we're already giving you a gumball");
+	}
+	public void turnCrank(){
+		System.out.println("Turning twice doesn't get you another gumball!");
+	} 
+	public void dispense(){
+		System.out.println("you are winner! you get two gumballs for your quarter");
+		gumballMachine.releaseBall();
+		if(gumballMachine.getCount()==0)
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		else 
+			gumballMachine.releaseBall();
+		if(gumballMachine.getCount()>0)
+			gumballMachine.setState(gumballMachine.getNoQuarterState());
+		else{
+			System.out.println("Oops, out of gumballs!");
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		}
+	}
+	public String toString(){
+		return "dispensing two gumballs for your quarter, because you are a winner";
+	}
+}
